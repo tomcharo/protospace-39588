e7e64501cb6a346136e8a7041b840e3e66c33d6a
tomcharo <shota.a_2807@softbank.ne.jp>
2023-10-02 22:47:24 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to prototype_path(comment.prototype_id)
+    else
+      @prototype = Prototype.find(comment.prototype_id)
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
